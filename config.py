bc0aa5c8802d8c7ed9ee1eed93dc49c716065741
mani-chowdary <94671414+mani-chowdary@users.noreply.github.com>
2025-05-01 21:26:50 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-apikey = os.getenv("OPENAI_API_KEY")
+apikey = os.getenv("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")
